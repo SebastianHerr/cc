df01e7ae3a729059c3a12babe0dddc4ce75d3e80
nineto <umnachtung@googlemail.com>
2017-05-23 21:57:22 +0200

    adding build script for bash

---
+++ src/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+PARSER='C0'
+
+if [ ! -f "bin/" ]
+then
+  mkdir bin
+fi
+
+javacc $PARSER.jj
+javac -d bin *.java
+
+echo -e "\n\e[1mBuilding completed. \e[0m \n"
+
+for testfile in test/*.grm
+do
+  echo -e "\e[1m$testfile:\e[0m"
+  java -cp bin $PARSER $testfile
+done
