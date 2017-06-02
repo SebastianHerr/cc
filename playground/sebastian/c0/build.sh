14cf518a83be60f8c67f463c286fe098c327f6a4
SebastianHerr <Sebastian-Herr@web.de>
2017-06-02 16:27:31 +0200

    Fixed Bugs and automated the process a bit more

--- playground/sebastian/c0/build.sh
+++ playground/sebastian/c0/build.sh
@@ -2,6 +2,8 @@
 
 PARSER='C0'
 
+source cleanFolder.sh
+
 if [ ! -d "bin/" ]
 then
   mkdir bin
@@ -9,3 +11,9 @@ fi
 
 javacc -OUTPUT_DIRECTORY=bin/ $PARSER.jj
 javac -d bin bin/*.java
+
+for testfile in test/*
+do
+  echo -e "\e[1m$testfile:\e[0m"
+  java -cp bin $PARSER $testfile
+done
