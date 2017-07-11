81f83666f8f00f5ff3dfab238d2c5e33955ac951
SebastianHerr <Sebastian-Herr@web.de>
2017-07-11 14:54:55 +0200

    Added some test automation output to the shell build and test script.

--- src/build.sh
+++ src/build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 PARSER='C0'
 
@@ -43,11 +43,49 @@ fi
 
 if test "$TESTP" ; then
   echo Parser will be tested with the filter "$FILTER"
+  PASSEDALLTEST=0
   for testfile in `find test -type f -name "$FILTER"`
   do
     echo -e "\e[1m$testfile:\e[0m"
-    java -cp bin $PARSER $testfile
+    java -cp bin $PARSER $testfile print
+    
+    #Checking of the return value to further automate the tests
+    RETURNVALUE=$?
+    if [ "$RETURNVALUE" -ne "0" ] 
+    then
+      if [[ $testfile == *"pass"* ]];
+      then
+        echo -e "\e[31m failed this test checking if good enough\e[39m"
+        #at this point the return value is != 0 but the test is called pass, some error occured.
+        #Now it needs to be tested that enough of the parser has done it's job
+        if [[ $testfile == *"parsing"* ]]  && [ "$RETURNVALUE" -lt "2" ]; 
+        then
+          #Failed in the parsing stage
+          echo  -e "\e[31m Test failed in the parsing stage\e[39m"
+          PASSEDALLTEST=1
+        else
+          if [[ $testfile == *"symbolTable"* ]]  && [ "$RETURNVALUE" -lt "3" ]; 
+          then
+            #failed in the symbol table stage
+            echo  -e "\e[31mTets failed in the symbol table stage\e[39m"
+            PASSEDALLTEST=1
+          else 
+            echo  -e "\e[32m This test failed, but outside of the scope of the test.\e[39m"
+          fi
+        fi
+      else
+        echo  -e "\e[32m Test failed as planned, not checking where it failed.\e[39m"
+      fi
+    fi
+    
   done
+  echo "#####################################################"
+  if [ "$PASSEDALLTEST" -eq "0" ]
+  then
+    echo -e "\e[32mPassed all tests.\e[39m"
+  else
+    echo -e "\e[31mOne or more tests failed\e[39m"
+  fi
 else 
   echo Parser not tested
 fi
